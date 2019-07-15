0813fefb7f6c0113867491490429b25c4f78f091
Coolmt3 <38844686+Coolmt3@users.noreply.github.com>
2019-07-15 14:15:30 -0500

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -27,18 +27,18 @@ function myFunction() {
   }
 }
 
-var checkboxValues = JSON.parse(localStorage.getItem('checkboxValues')) || {},
-    $checkboxes = $("#checkbox-container :checkbox");
-
-$checkboxes.on("change", function(){
-  $checkboxes.each(function(){
-    checkboxValues[this.id] = this.checked;
-  });
-  
-  localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
+var checkboxValues = {};
+$(":checkbox").each(function(){
+  checkboxValues[this.id] = this.checked;
 });
+$.cookie('checkboxValues', checkboxValues, { expires: 7, path: '/' })
 
-// On page load
-$.each(checkboxValues, function(key, value) {
-  $("#" + key).prop('checked', value);
-});
+function repopulateCheckboxes(){
+  var checkboxValues = $.cookie('checkboxValues');
+  if(checkboxValues){
+    Object.keys(checkboxValues).forEach(function(element) {
+      var checked = checkboxValues[element];
+      $("#" + element).prop('checked', checked);
+    });
+  }
+}
