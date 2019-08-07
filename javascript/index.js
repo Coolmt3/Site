876613092c58d5f74519e4890c4798e5bc8935c2
Coolmt3 <38844686+Coolmt3@users.noreply.github.com>
2019-08-07 09:34:13 -0500

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,14 @@
-document.getElementById('date').innerHTML = new Date().toDateString();
 
 
+function getDate() {
+  var d = new Date();
+  var n = d.toDateString();
+  document.getElementById("date").innerHTML = n;
+}
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "200px";
 }
 
 function closeNav() {
